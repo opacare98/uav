41181d7f3c84e4bd8d3d7a5ef975c0c30d5b9a4c
储诗韵 <chushiyun@chushiyundeMacBook-Pro.local>
2023-10-16 14:40:55 +0800

    Initial comment

---
+++ uav.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 25 08:42:59 2023
+
+@author: chushiyun
+"""
+
+from ultralytics import YOLO
+import cv2
+import time
+
+# Load the model.
+model = YOLO('yolov8n.pt')
+ 
+# Training.
+results = model.train(
+   data='uav.yaml',
+   imgsz=640,
+   epochs=10,
+   batch=5,
+   name='yolov8n_v8_50e'
+)
+
